767f69c6dc96b3b26772ebd14d9cf441d47131cb
Jerry Wu <jerrywu12@gmail.com>
2017-03-15 17:08:36 -0700

    refactoring - update comments and re-indent

--- App/Containers/App.js
+++ App/Containers/App.js
@@ -29,7 +29,7 @@ applyConfigSettings();
 const store = createStore();
 
 /**
- * Provides an entry point into our application.  Both index.ios.js and index.android.js
+ * Provides an entry points into demo application.  Both index.ios.js and index.android.js
  * call this component first.
  *
  * We create our Redux store here, put it into a provider and then bring in our
@@ -39,119 +39,119 @@ const store = createStore();
  */
 class App extends Component {
 
-  constructor() {
-    super();
-    // store.dispatch(LoginActions.login('lwz2', '1'))
-    // store.dispatch(LoginActions.login('w', 'w'))
+    constructor() {
+        super();
+        // store.dispatch(LoginActions.login('apple', 'apple'));    // example account
 
-    WebIM.conn.listen({
-      // xmpp connected successfully
-      onOpened: (msg) => {
-        // send notification if presence
-        WebIM.conn.setPresence();
-        // get contacts
-        store.dispatch(RosterActions.getContacts());
-        // login succeed
-        store.dispatch(LoginActions.loginSuccess(msg));
-        // get blacklist
-        store.dispatch(BlacklistActions.getBlacklist());
-        // get group list
-        store.dispatch(GroupActions.getGroups());
+        WebIM.conn.listen({
+            // xmpp connected successfully
+            onOpened: (msg) => {
+                // send notification if presence
+                WebIM.conn.setPresence();
+                // get contacts
+                store.dispatch(RosterActions.getContacts());
+                // login succeed
+                store.dispatch(LoginActions.loginSuccess(msg));
+                // get blacklist
+                store.dispatch(BlacklistActions.getBlacklist());
+                // get group list
+                store.dispatch(GroupActions.getGroups());
 
-        NavigationActions.contacts();
-      },
+                NavigationActions.contacts();
+            },
 
-      // presence status
-      onPresence: (msg) => {
-        console.debug('onPresence', msg, store.getState());
-        switch (msg.type) {
-          case 'subscribe':
-            // Add friend is a process of two ways subscription
-            // if both parties agree to add each other as friend, then one party will subscribe automatically
-            // Does not need notification, indicated as state=[resp:true]
-            if (msg.status === '[resp:true]') {
-              return;
+            // presence status
+            onPresence: (msg) => {
+                console.debug('onPresence', msg, store.getState());
+                switch (msg.type) {
+                    case 'subscribe':
+                        // Add friend is a process of two ways subscription
+                        // if both parties agree to add each other as friend, then one party will subscribe automatically
+                        // Does not need notification, indicated as state=[resp:true]
+                        if (msg.status === '[resp:true]') {
+                            return;
+                        }
+                        store.dispatch(SubscribeActions.addSubscribe(msg));
+                        break;
+                    case 'subscribed':
+                        store.dispatch(RosterActions.getContacts());
+                        Alert.alert(msg.from + ' ' + I18n.t('subscribed'));
+                        break;
+                    case 'unsubscribe':
+                        // TODO: partial UI refresh
+                        store.dispatch(RosterActions.getContacts());
+                        break;
+                    case 'unsubscribed':
+                        // un-friended
+                        store.dispatch(RosterActions.getContacts());
+                        Alert.alert(msg.from + ' ' + I18n.t('unsubscribed'));
+                        break;
+                }
+            },
+
+            // Errors
+            onError: (error) => {
+                console.log(error);
+
+                // 16: server-side close the websocket connection
+                if (error.type == WebIM.statusCode.WEBIM_CONNCTION_DISCONNECTED) {
+                    console.log('WEBIM_CONNCTION_DISCONNECTED', WebIM.conn.autoReconnectNumTotal, WebIM.conn.autoReconnectNumMax);
+                    if (WebIM.conn.autoReconnectNumTotal < WebIM.conn.autoReconnectNumMax) {
+                        return;
+                    }
+                    Alert.alert('Error', 'server-side close the websocket connection');
+                    NavigationActions.login();
+                    return;
+                }
+                // 8: logout due to l
+                if (error.type == WebIM.statusCode.WEBIM_CONNCTION_SERVER_ERROR) {
+                    console.log('WEBIM_CONNCTION_SERVER_ERROR');
+                    Alert.alert('Error', 'offline by multi login');
+                    NavigationActions.login();
+                    return;
+                }
+                // login error
+                if (error.type == 1) {
+                    let data = error.data ? error.data.data : '';
+                    data && Alert.alert('Error', data);
+                    store.dispatch(LoginActions.loginFailure(error));
+                }
+            },
+
+            // disconnected
+            onClosed: (msg) => {
+                console.log('onClosed');
+            },
+
+            // blacklist updated
+            onBlacklistUpdate: (list) => {
+                store.dispatch(BlacklistActions.updateBlacklist(list));
+            },
+
+            // text message received
+            onTextMessage: (message) => {
+                console.log('onTextMessage', message);
+                store.dispatch(MessageActions.addMessage(message, 'txt'));
+            },
+
+            // picture message received
+            onPictureMessage: (message) => {
+                console.log('onPictureMessage', message);
+                store.dispatch(MessageActions.addMessage(message, 'img'));
             }
-            store.dispatch(SubscribeActions.addSubscribe(msg));
-            break;
-          case 'subscribed':
-            store.dispatch(RosterActions.getContacts());
-            Alert.alert(msg.from + ' ' + I18n.t('subscribed'));
-            break;
-          case 'unsubscribe':
-            // TODO: partial UI refresh
-            store.dispatch(RosterActions.getContacts());
-            break;
-          case 'unsubscribed':
-            // un-friended
-            store.dispatch(RosterActions.getContacts());
-            Alert.alert(msg.from + ' ' + I18n.t('unsubscribed'));
-            break;
-        }
-      },
+        })
+    }
 
-      // Errors
-      onError: (error) => {
-        console.log(error);
+    componentWillReceiveProps() {
+    }
 
-        // 16: server-side close the websocket connection
-        if (error.type == WebIM.statusCode.WEBIM_CONNCTION_DISCONNECTED) {
-          console.log('WEBIM_CONNCTION_DISCONNECTED', WebIM.conn.autoReconnectNumTotal, WebIM.conn.autoReconnectNumMax);
-          if (WebIM.conn.autoReconnectNumTotal < WebIM.conn.autoReconnectNumMax) {
-            return;
-          }
-          Alert.alert('Error', 'server-side close the websocket connection');
-          NavigationActions.login();
-          return;
-        }
-        // 8: offline by multi login
-        if (error.type == WebIM.statusCode.WEBIM_CONNCTION_SERVER_ERROR) {
-          console.log('WEBIM_CONNCTION_SERVER_ERROR');
-          Alert.alert('Error', 'offline by multi login');
-          NavigationActions.login();
-          return;
-        }
-        if (error.type == 1) {
-          let data = error.data ? error.data.data : '';
-          data && Alert.alert('Error', data);
-          store.dispatch(LoginActions.loginFailure(error));
-        }
-      },
-
-      // disconnected
-      onClosed: (msg) => {
-        console.log('onClosed');
-      },
-
-      // update blacklist
-      onBlacklistUpdate: (list) => {
-        store.dispatch(BlacklistActions.updateBlacklist(list));
-      },
-
-      // text message
-      onTextMessage: (message) => {
-        console.log('onTextMessage', message);
-        store.dispatch(MessageActions.addMessage(message, 'txt'));
-      },
-
-      // picture message
-      onPictureMessage: (message) => {
-        console.log('onPictureMessage', message);
-        store.dispatch(MessageActions.addMessage(message, 'img'));
-      }
-    })
-  }
-
-  componentWillReceiveProps() {
-  }
-
-  render() {
-    return (
-      <Provider store={store}>
-        <RouterWithRedux />
-      </Provider>
-    )
-  }
+    render() {
+        return (
+            <Provider store={store}>
+              <RouterWithRedux />
+            </Provider>
+        )
+    }
 }
 
 export default App
--- index.ios.js
+++ index.ios.js
@@ -4,4 +4,4 @@ import './App/Config/ReactotronConfig'
 import { AppRegistry } from 'react-native'
 import App from './App/Containers/App'
 
-AppRegistry.registerComponent('app', () => App)
+AppRegistry.registerComponent('app', () => App);
